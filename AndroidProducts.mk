686722c7da5701527d023b84cc33ba3f674705e7
leo <87917944+leole0leoleo@users.noreply.github.com>
2022-01-29 17:03:06 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_j4lte.mk
+    $(LOCAL_DIR)/lineage_j4lte.mk
 
 COMMON_LUNCH_CHOICES := \
-     havoc_j4lte-userdebug \
-     havoc_j4lte-eng \
-     havoc_j4lte-user
+     lineage_j4lte-userdebug \
+     lineage_j4lte-eng \
+     lineage_j4lte-user
